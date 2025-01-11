fa685190ab4788b78af6b84197d0f05ba19c707d
cyapharis <cyapharis@student.unimelb.edu.au>
2025-01-12 00:52:22 +1100

    Utilize matplotlib to visualize image labels

--- Rekognition.py
+++ Rekognition.py
@@ -1,4 +1,8 @@
 import boto3
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from io import BytesIO
+from PIL import Image
 
 bucket_name = ''
 image_key = ''
@@ -15,28 +19,57 @@ def detect_labels(photo, bucket):
 
     response = client.detect_labels(
         Image={'S3Object':{'Bucket':bucket_name,'Name':image_key}},
-        MaxLabels=10)
+        MaxLabels=10,
+        MinConfidence=70
+    )
 
     print('Detected labels for ' + photo) 
-    print()   
-    for label in response['Labels']:
-        print ("Label: " + label['Name'])
-        print ("Confidence: " + str(label['Confidence']))
-        print ("Instances:")
-        for instance in label['Instances']:
-            print ("  Bounding box")
-            print ("    Top: " + str(instance['BoundingBox']['Top']))
-            print ("    Left: " + str(instance['BoundingBox']['Left']))
-            print ("    Width: " +  str(instance['BoundingBox']['Width']))
-            print ("    Height: " +  str(instance['BoundingBox']['Height']))
-            print ("  Confidence: " + str(instance['Confidence']))
-            print()
+    print()
 
-        print ("Parents:")
-        for parent in label['Parents']:
-            print ("   " + parent['Name'])
-        print ("----------")
-        print ()
+    # Fetch image from S3 for plotting
+    s3_client = boto3.client(service_name='s3', region_name='us-east-1',
+                         aws_access_key_id=access_key,
+                         aws_secret_access_key=secret_key)
+    s3_response = s3_client.get_object(Bucket=bucket_name, Key=image_key)
+    image_data = s3_response['Body'].read()
+    image_pil = Image.open(BytesIO(image_data))
+
+    # Convert the image to an RGB format that can be displayed by Matplotlib
+    image_rgb = image_pil.convert('RGB')
+
+    # Plot the image using matplotlib
+    fig, ax = plt.subplots(1, figsize=(12, 8))
+    ax.imshow(image_rgb)
+
+
+    for label in response['Labels']:
+       for instance in label['Instances']:
+            # Get the bounding box coordinates (normalized)
+            bbox = instance['BoundingBox']
+            left = bbox['Left'] * image_rgb.width
+            top = bbox['Top'] * image_rgb.height
+            width = bbox['Width'] * image_rgb.width
+            height = bbox['Height'] * image_rgb.height
+
+            # Create a Rectangle patch for the bounding box
+            rect = patches.Rectangle(
+                (left, top), width, height, linewidth=2, edgecolor='red', facecolor='none'
+            )
+
+            # Add the rectangle to the plot
+            ax.add_patch(rect)
+
+            # Add label text next to the bounding box
+            ax.text(
+                left, top, label['Name'], color='red', fontsize=12,
+                verticalalignment='bottom', horizontalalignment='left'
+            )
+
+    # Hide axes and show the image
+    ax.axis('off')
+    plt.show()
+
+    # Return the number of labels detected
     return len(response['Labels'])
 
 
